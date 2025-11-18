db1b7bc2ba6759071c9f157eac1ab600a31d1fb0
Liza <yelyzavetafesina@gmail.com>
2025-11-18 16:43:30 +0100

    feat: working progress of fetching data form supabase

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,17 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Stack } from '@mui/material'
+import { supabase } from '../lib/supabaseClient.ts'
 
 export const Route = createFileRoute('/')({
+  loader: () => supabase.from('animals').select(),
   component: Index,
+  pendingComponent: () => <div>Animals Loading...</div>,
+  pendingMs: 0,
 })
 
 function Index() {
+  const data = Route.useLoaderData()
+  console.log({ data })
   return (
     <Stack padding={2}>
       <h3>Welcome Home!</h3>
